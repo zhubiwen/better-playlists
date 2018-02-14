dbc5dea21e42e0c367265e83b60c4fea29d9b94e
Biwen <biwen1209@gmail.com>
2018-02-14 14:46:59 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultTextColor = '#fff'
+let defaultStyle = {
+  color: defaultTextColor
+}
+class Aggregate extends Component {
+  render(){
+    return(
+      <div style = {{...defaultStyle, width:"40%", display: "inline-block"}}>
+        <h2 > Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component{
+  render(){
+    return (
+      <div style ={defaultStyle}>
+        <img/>
+        <input type = 'text'/>
+        Filter
+      </div>
+    )
+  }
+}
+
+class PlayList extends Component {
+  render() {
+    return (
+      <div style ={{...defaultStyle, display:'inline-block', width:"25%"}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    let name = 'David'
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <PlayList/>
+        <PlayList/>
+        <PlayList/>
+        <PlayList/>
+      </div>
+    );
+  }
+}
+
+export default App;
