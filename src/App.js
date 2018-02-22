839cb5c054570f977f6c31eafb673b33fc5ccae2
Biwen <biwen1209@gmail.com>
2018-02-22 11:45:29 -0600

    a

--- src/App.js
+++ src/App.js
@@ -103,6 +103,9 @@ class App extends Component {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
 
+    if(!accessToken)
+    return;
+
     fetch ('https://api.spotify.com/v1/me', {
         headers: {'Authorization': 'Bearer ' + accessToken}
     }).then(response=> response.json())
